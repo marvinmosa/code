d3fb95f1ff720023050787f64c0807bc169c9284
Jan Marvin Mosa <marvinmosa@gmail.com>
2017-09-11 10:32:07 +0800

    added showError function

--- app/src/main/java/com/mower/TemplateActivity.java
+++ app/src/main/java/com/mower/TemplateActivity.java
@@ -1,14 +1,20 @@
 package com.mower;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by marvinmosa on 9/11/17.
@@ -66,5 +72,27 @@ public class TemplateActivity extends AppCompatActivity {
             }
             parent = parent.getParent();
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    releaseFocus(v);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    public void showError(String body) {
+        Toast.makeText(this, body, Toast.LENGTH_SHORT).show();
     }
 }
